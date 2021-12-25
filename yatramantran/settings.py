e6c70c2b697798cff182b9275ce53077fc65794a
HITANSHU PAREKH <50589328+hparekh72@users.noreply.github.com>
2021-12-25 23:01:32 +0530

    Update settings.py

--- yatramantran/settings.py
+++ yatramantran/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,10 +88,6 @@ DATABASES = {
         'PORT' : '',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
