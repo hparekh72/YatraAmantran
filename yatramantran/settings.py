e8516d3a3073a46e0c9205a214d9415a47d6d4cb
HITANSHU PAREKH <hitanshuparekh72@gmail.com>
2021-12-26 13:05:46 +0530

    Teams update

--- yatramantran/settings.py
+++ yatramantran/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -137,11 +136,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
